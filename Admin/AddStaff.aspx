e53b0d68b19fefe96e2ea1a89aef47e32199e4d0
wuliaosile <konghang97@gmai.com>
2024-05-04 05:03:20 +0800

    New create more using flex box and grid

---
+++ Admin/AddStaff.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="AddStaff.aspx.cs" Inherits="HotelManagement.Admin.AddStaff" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <style>
+        .wrapper {
+            display:flex;
+            justify-content: center;
+        }
+
+        .card_staff {
+            width: 500px;
+            height:600px;
+            top:50px;
+            border-radius:35px;
+        }
+        .card_staff h3{
+            text-align:center;
+        }
+    </style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+        <div class="card card_staff">
+            <div class="card-body">
+                <h3 class="card-title">Add Staff</h3>
+
+            </div>
+        </div>
+
+</asp:Content>
---
+++ AutoGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Web;
+
+namespace HotelManagement
+{
+    public class AutoGenerator
+    {
+        //check valid password
+        public bool IsValidPassword(string password)
+        {
+            Regex regex = new Regex(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+            return regex.IsMatch(password);
+        }
+        //generate salt
+        public static string GenerateSalt()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                var salt = new byte[32];
+                rng.GetBytes(salt);
+                return Convert.ToBase64String(salt);
+            }
+        }
+        //generate hashed password
+        public static string GenerateHashedPassword(string password, string salt)
+        {
+            byte[] saltBytes = Convert.FromBase64String(salt);
+            Rfc2898DeriveBytes pbkdf2 = new Rfc2898DeriveBytes(password, saltBytes, 1000);
+            byte[] hashBytes = pbkdf2.GetBytes(256 / 8);
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                builder.Append(hashBytes[i].ToString("x2"));
+            }
+            return builder.ToString();
+        }
+    }
+}
